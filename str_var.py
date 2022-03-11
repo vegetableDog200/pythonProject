d639302635fff286203187cab0a21f603f410295
redRockmengxin <1065164542@qq.com>
2022-03-11 17:28:23 +0800

    str_var.py

---
+++ str_var.py
@@ -0,0 +1,7 @@
+# it's str var exercise
+message = "hello yizhou li. welcome to python world!"
+print(message.title())
+
+first_name = "Jack"
+last_name = "Loosen"
+print((first_name+" "+last_name).upper())
